9542caefed3f12716a481c5aa3b1933f3a3e4bbb
Dr.hossein Shabani <shabanimehran@gmail.com>
2025-08-29 05:29:38 +0330

    Update routers.py

--- api/routers.py
+++ api/routers.py
@@ -2,16 +2,10 @@ from django.urls import path, include
 from django.http import JsonResponse, HttpRequest
 from django.views.decorators.http import require_safe
 from django.views.decorators.cache import never_cache
-from rest_framework import routers
 from api.versions import VersionViewSet
 
 app_name = "api"
 
-# Create DRF router
-router = routers.DefaultRouter()
-
-# Add version viewset - but don't register it as it has custom routes
-# router.register(r'versions', VersionViewSet, basename='version')
 
 @never_cache
 @require_safe
@@ -25,6 +19,14 @@ version_revert = VersionViewSet.as_view({'post': 'revert'})
 
 urlpatterns = [
     path('health/', health, name='health'),
-    path('versions/<str:resource_type>/<uuid:resource_id>/', version_list, name='version-list'),
-    path('versions/<str:resource_type>/<uuid:resource_id>/revert/', version_revert, name='version-revert'),
-]
+    path(
+        'versions/<str:resource_type>/<uuid:resource_id>/',
+        version_list,
+        name='version-list',
+    ),
+    path(
+        'versions/<str:resource_type>/<uuid:resource_id>/revert/',
+        version_revert,
+        name='version-revert',
+    ),
+ ]
