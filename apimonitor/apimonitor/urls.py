ac09e0e617cc9f1f19b18b6b0a82a5b316cd16dd
Dr.hossein Shabani <shabanimehran@gmail.com>
2025-09-06 17:32:51 +0330

    Update urls.py

--- apimonitor/apimonitor/urls.py
+++ apimonitor/apimonitor/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/monitor/', include('monitor.urls')),
+    path('api/monitor/', include('monitor.urls', namespace='monitor'))
 ]
